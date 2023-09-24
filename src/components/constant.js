fd2efd45304e953cd45c93281dc745e18ca25975
abhishek <abhiy145@gmail.com>
2023-09-24 22:21:45 +0530

    education added

--- src/components/constant.js
+++ src/components/constant.js
@@ -1,37 +1,78 @@
 import here from "../assets/icon/here.svg";
 import Lti from "../assets/icon/Lti.svg";
+import nm from "../assets/icon/nm.svg";
+import srmv from "../assets/icon/srmv.svg";
+import tp from "../assets/icon/tp.svg";
 
 export const experiences = [
-    {
-      title: "Software Engineer II",
-      company_name: "Here Technologies",
-      icon: here,
-      iconBg: "#d1d5db",
-      date: "Feb 2023 - Present",
-      points: [
-        "Developed and maintained responsive web applications using React.js and Redux for efficient state management.",
-        "Used ReactJS, Redux, and Thunk to create robust and performant frontend applications that meet business and user needs.",
-        "Collaborated with cross-functional teams, including designers and backend developers, to deliver high-quality products.",
-        "Conducted thorough unit and end-to-end testing to identify and resolve bugs, ensuring a seamless user experience.",
-        "Optimized user experience and increased customer engagement by 40% through A/B testing and UI redesigns",
-        "Actively participated in Agile development processes, providing valuable input and adapting to a fast-paced environment.",
-        "Worked with CI/CD and containerization technologies like Docker and Kubernetes to streamline the development, deployment, and maintenance of applications.",
-      ],
-    },
-    {
-      title: "Senior Software Engineer",
-      company_name: "L&T Infotech",
-      icon: Lti,
-      iconBg: "#E6DEDD",
-      date: "Oct 2019 - Jan 2023",
-      points: [
-        "Architected front-end functionality for e-commerce sites; integrated dynamic payment gateways and increased checkout conversion rates by 25%.",
-        "Implemented responsive designs and mobile-first development strategies to enhance user experience.",
-        "Implemented TypeScript for type checking, resulting in more robust and maintainable code.",
-        "Collaborated closely with the engineering team across different time zones, enabling effective global project development.",
-        "Conducted thorough testing and debugging to ensure the quality and reliability of the applications.",
-        "Incorporated version control with GIT to streamline collaborative development and to maintain codebase integrity.",
-        "Improved the performance and load times of web applications through optimization techniques.",
-      ],
-    },
-  ];
+  {
+    title: "Software Engineer II",
+    company_name: "Here Technologies",
+    icon: here,
+    iconBg: "#d1d5db",
+    date: "Feb 2023 - Present",
+    points: [
+      "Developed and maintained responsive web applications using React.js and Redux for efficient state management.",
+      "Used ReactJS, Redux, and Thunk to create robust and performant frontend applications that meet business and user needs.",
+      "Collaborated with cross-functional teams, including designers and backend developers, to deliver high-quality products.",
+      "Conducted thorough unit and end-to-end testing to identify and resolve bugs, ensuring a seamless user experience.",
+      "Optimized user experience and increased customer engagement by 40% through A/B testing and UI redesigns",
+      "Actively participated in Agile development processes, providing valuable input and adapting to a fast-paced environment.",
+      "Worked with CI/CD and containerization technologies like Docker and Kubernetes to streamline the development, deployment, and maintenance of applications.",
+    ],
+  },
+  {
+    title: "Senior Software Engineer",
+    company_name: "L&T Infotech",
+    icon: Lti,
+    iconBg: "#E6DEDD",
+    date: "Oct 2019 - Jan 2023",
+    points: [
+      "Architected front-end functionality for e-commerce sites; integrated dynamic payment gateways and increased checkout conversion rates by 25%.",
+      "Implemented responsive designs and mobile-first development strategies to enhance user experience.",
+      "Implemented TypeScript for type checking, resulting in more robust and maintainable code.",
+      "Collaborated closely with the engineering team across different time zones, enabling effective global project development.",
+      "Conducted thorough testing and debugging to ensure the quality and reliability of the applications.",
+      "Incorporated version control with GIT to streamline collaborative development and to maintain codebase integrity.",
+      "Improved the performance and load times of web applications through optimization techniques.",
+    ],
+  },
+];
+
+export const education = [
+  {
+    title: "Narsee Monjee College",
+    company_name: "B.sc. Information Technology",
+    icon: nm,
+    iconBg: "#E6DEDD",
+    date: "June 2016 - May 2019",
+    points: [
+      "Board: Maharashtra State Board of Secondary & Higher Secondary Education",
+      "CGPA: 7.5/10",
+    ],
+  },
+
+  {
+    title: "T.P. Bhatia College",
+    company_name: "Class 12th (Senior Secondary)",
+    icon: tp,
+    iconBg: "#E6DEED",
+    date: "June 2014 - May 2015",
+    points: [
+      "Board: Maharashtra State Board of Secondary & Higher Secondary Education",
+      "Percentage: 83%",
+    ],
+  },
+
+  {
+    title: "S.R.M.V.",
+    company_name: "Class 10th (Secondary)",
+    icon: srmv,
+    iconBg: "#d1d5db",
+    date: "June 2012 - March 2023",
+    points: [
+      "Board: Maharashtra State Board of Secondary & Higher Secondary Education",
+      "Percentage: 83%",
+    ],
+  },
+];
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,58 +1,135 @@
+import { useState } from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiences, education } from "../components/constant";
 
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import { experiences } from '../components/constant';
+const ExperienceTimeline = ({ id }) => {
+  const [activeTab, setActiveTab] = useState("work"); // Default to 'work' tab
 
-
-
-const ExperienceTimeline = ({id}) => {
   return (
     <div id={id} className="container mx-auto">
-      <h2 className="text-3xl font-semibold mb-6 text-pblue">
-         Work Experience
-        </h2>
-    <VerticalTimeline animate>
-       
-      {experiences.map((experience, index) => (
-        <VerticalTimelineElement
-          key={index}
-          className="vertical-timeline-element"
-          contentStyle={{
-            background: experience.iconBg,
-            color: "#000",
-          }}
-          contentArrowStyle={{
-            borderRight: `7px solid ${experience.iconBg}`,
-          }}
-          date={experience.date}
-          iconStyle={{
-            background: experience.iconBg,
-            color: '#fff',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: '50%', 
-            width: '60px', 
-            height: '60px',
-          }}
-          icon={<img src={experience.icon} style={{ maxWidth: '100%', maxHeight: '100%' }} alt="Company Icon" />}
+      <div className="flex justify-between">
+        <button
+          className={`text-xl font-semibold mb-6 text-pblue ${
+            activeTab === "work" ? "eudcation" : "work"
+          }`}
+          onClick={() => setActiveTab("work")}
         >
-           <h4 className="vertical-timeline-element-subtitle font-bold">
-  {experience.company_name} <span className='ml-5'> | </span>
-  <span className="vertical-timeline-element-title font-light ml-5">
-    {experience.title}
-  </span>
-</h4>
-         
-         
-          <ul className="list-disc pl-5">
-            {experience.points.map((point, idx) => (
-              <li key={idx} className='info-text'>{point}</li>
-            ))}
-          </ul>
-        </VerticalTimelineElement>
-      ))}
-    </VerticalTimeline>
+          Work Experience
+        </button>
+        <button
+          className={`text-xl font-semibold mb-6 text-pblue ${
+            activeTab === "education" ? "underline" : ""
+          }`}
+          onClick={() => setActiveTab("education")}
+        >
+          Education
+        </button>
+      </div>
+      {activeTab === "work" && (
+        <VerticalTimeline animate>
+          {experiences.map((experience, index) => (
+            <VerticalTimelineElement
+              key={index}
+              className="vertical-timeline-element"
+              contentStyle={{
+                background: experience.iconBg,
+                color: "#000",
+              }}
+              contentArrowStyle={{
+                borderRight: `7px solid ${experience.iconBg}`,
+              }}
+              date={experience.date}
+              iconStyle={{
+                background: experience.iconBg,
+                color: "#fff",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "50%",
+                width: "60px",
+                height: "60px",
+              }}
+              icon={
+                <img
+                  src={experience.icon}
+                  style={{ maxWidth: "100%", maxHeight: "100%" }}
+                  alt="Company Icon"
+                />
+              }
+            >
+              <h4 className="vertical-timeline-element-subtitle font-bold">
+                {experience.company_name} <span className="ml-5"> | </span>
+                <span className="vertical-timeline-element-title font-light ml-5">
+                  {experience.title}
+                </span>
+              </h4>
+
+              <ul className="list-disc pl-5">
+                {experience.points.map((point, idx) => (
+                  <li key={idx} className="info-text">
+                    {point}
+                  </li>
+                ))}
+              </ul>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      )}
+
+      {activeTab === "education" && (
+        <VerticalTimeline animate>
+          {education.map((experience, index) => (
+            <VerticalTimelineElement
+              key={index}
+              className="vertical-timeline-element"
+              contentStyle={{
+                background: experience.iconBg,
+                color: "#000",
+              }}
+              contentArrowStyle={{
+                borderRight: `7px solid ${experience.iconBg}`,
+              }}
+              date={experience.date}
+              iconStyle={{
+                background: experience.iconBg,
+                color: "#fff",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "50%",
+                width: "60px",
+                height: "60px",
+              }}
+              icon={
+                <img
+                  src={experience.icon}
+                  style={{ maxWidth: "100%", maxHeight: "100%" }}
+                  alt="Company Icon"
+                />
+              }
+            >
+              <h4 className="vertical-timeline-element-subtitle font-bold">
+                {experience.company_name} <span className="ml-5"> | </span>
+                <span className="vertical-timeline-element-title font-light ml-5">
+                  {experience.title}
+                </span>
+              </h4>
+
+              <ul className="list-disc pl-5">
+                {experience.points.map((point, idx) => (
+                  <li key={idx} className="info-text">
+                    {point}
+                  </li>
+                ))}
+              </ul>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      )}
     </div>
   );
 };
